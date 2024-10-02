afd8683ac5e324570a63c7f86009a4025c294680
eustachekamala <eustachejared25@gmail.com>
2024-10-02 12:33:42 +0300

    Putting it All Together: ORMs to manage our data base

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,25 @@
 from models import Dog
+from sqlalchemy.orm import Session # type: ignore
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
-def save(session, dog):
-    pass
+def save(session:Session, dog:Dog):
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
-def find_by_name(session, name):
-    pass
-
-def find_by_id(session, id):
-    pass
-
-def find_by_name_and_breed(session, name, breed):
-    pass
+def find_by_name(session:Session, name:str):
+    return session.query(Dog).filter(Dog.name == name).first()
+    
+def find_by_id(session:Session, id:int):
+    return session.query(Dog).filter(Dog.id == id).first()
+    
+def find_by_name_and_breed(session:Session, name:str, breed:str):
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).filter(Dog.id == dog.id).update({'breed': breed})
+    session.commit()
